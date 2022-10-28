0f41313e28c8358de002be316a0b530a6509f67b
Eber Herrera <eber.herrera@xing.com>
2022-10-28 13:28:08 +0200

    end of session II

--- src/async/index.ts
+++ src/async/index.ts
@@ -21,7 +21,7 @@ export const callAPIPromisified = (request: string): Promise<string> => {
         setTimeout(
             () => {
                 const response = callSyncInternet(request)
-    
+
                 resolve(response);
             }, 
             1000
@@ -29,4 +29,67 @@ export const callAPIPromisified = (request: string): Promise<string> => {
     });
 }
 
+export const callAPIMyPromisified = (request: string): MyPromise => {
+    const myPromise = new MyPromise();
 
+    setTimeout(
+        () => {
+            const response = callSyncInternet(request)
+
+            myPromise.resolve(response);
+        }, 
+        1000
+    );
+
+    return myPromise;
+}
+
+
+
+class MyPromise {
+    value:any = null;
+    thenCb:any = null;
+    catchCb: any = null;
+    thenPromise:any = null;
+
+    constructor() {
+
+    }
+
+    then(thenCb: any) {
+        this.thenCb = thenCb
+        this.thenPromise = new MyPromise()
+
+        return this.thenPromise
+    }
+
+    catch(catchCb: any) {
+        this.catchCb = catchCb;
+        this.thenPromise = new MyPromise()
+
+        return this.thenPromise
+    }
+
+    resolve(value: any) {
+        this.value = value
+        this.loop()
+    }
+
+    loop() {
+        if (this.thenPromise) {
+            let thenCbResult = null;
+            try {
+                thenCbResult = this.thenCb(this.value);
+            } catch(e) {
+                this.catchCb(e)
+            }
+            
+            if (thenCbResult instanceof MyPromise) {
+                this.thenPromise.then()        
+            }
+            this.thenPromise.resolve(this.thenCb(this.value))
+        } else {
+            return this.thenCb && this.thenCb(this.value)
+        }
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 
-import { callAPI, callAPIPromisified } from "./async"
+import { callAPI, callAPIMyPromisified, callAPIPromisified } from "./async"
 
 const lessons = {
 "modules": {
@@ -8,9 +8,10 @@ const lessons = {
 },
 "Array and Object destructuring" : 1,
 "Arrow functions": 1,
-"Promises" : 0,
-    "Event Loop": 0,
-    "Async Await": 0,
+"Promises" : 1,
+    "Event Loop": 1, //send link to documentation
+    "Callbacks to promises": 1,
+    "Async Await": 1,
 "Data Structures": 0,
     "Map": 0, 
     "Set": 0,
@@ -24,6 +25,8 @@ const lessons = {
 
 
 console.log('start')
+
+/*
 callAPI('sync 1', (response: string) => {
     console.log(response)
     callAPI(response + 'sync 2', (response: string) => {
@@ -34,6 +37,9 @@ callAPI('sync 1', (response: string) => {
                 console.log(response)
                 callAPI(response + 'sync 5', (response: string) => {
                     console.log(response)
+                    callAPI(response + 'sync 5', (response: string) => {
+                        console.log(response)
+                    })
                 })
             })
         })
@@ -41,20 +47,120 @@ callAPI('sync 1', (response: string) => {
 });
 console.log('end')
 
-callAPIPromisified('sync 1')
+
+
+
+
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const myFunction = () => {
+    const untilSecondCall = callAPIPromisified('sync 1')
     .then((response: string) => {
         console.log(response)
-        return callAPIPromisified(response + 'sync 2')
+        throw "my error 2"
+        return callAPIPromisified(response + ' - sync 2')
     })
-    .then((response: string) => {
-        console.log(response)
-        return callAPIPromisified(response + 'sync 3')
+
+
+
+    untilSecondCall.then((response: string) => {
+            console.log(response)
+            //throw "my error 3"
+            return callAPIPromisified(response + ' - sync 3')
+        })
+        .then((response: string) => {
+            console.log(response)
+            return callAPIPromisified(response + ' - sync 4')
+        })
+        .then((response: string) => {
+            console.log(response)
+        })
+        .catch((e) => {
+            console.log('yesssss ' + e)
+            return 'managing error here';
+        })
+
+
+    untilSecondCall.catch((e) => {
+        return callAPIPromisified(e + ' - sync 3')
     })
+}
+
+
+const myFunctionAsyncAwait = async () => {
+    try {
+        const response1 = await callAPIPromisified('sync 1')
+        console.log(response1)
+        
+        let response2 = ''
+        try {
+            response2 = await callAPIPromisified(response1 + ' - sync 2')
+            console.log(response2)
+        } catch(e) {
+            console.log(response1 + ' - ' + e + ' - sync 2')
+        }
+        
+
+        const response3 = await callAPIPromisified(response2 + ' - sync 3')
+        console.log(response3)
+
+        const response4 = await callAPIPromisified(response3 + ' - sync 4')
+        console.log(response4)
+
+        return response4
+    } catch (e) {
+        console.log('yesssss ' + e)
+        return 'managing error here';
+    }
+}
+
+
+
+const fn = async () => {
+    const response4 = await myFunctionAsyncAwait()
+}
+
+
+
+
+
+
+
+    
+    /*
     .then((response: string) => {
-        console.log(response)
-        return callAPIPromisified(response + 'sync 4')
-    }).then((response: string) => {
         console.log(response)
         return callAPIPromisified(response + 'sync 6')
     })
-    .then((response) => console.log(response))
+    
+    .then((response: any) => console.log(response))
+
+*/
